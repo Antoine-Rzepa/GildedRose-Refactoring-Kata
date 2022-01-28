c50b4921be4fd0f25ba1e64ac874fe651d4d4fad
Antoine-Rzepa <antoine.rzepa@le-campus-numerique.fr>
2022-01-28 11:01:51 +0100

    Nouveau produit Conjured avec règles spécifique ajoutée

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,5 +1,5 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,57 +7,73 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      // Cas communs -1 quality
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
+            if (this.items[i].name != 'Conjured') {
+              this.items[i].quality = this.items[i].quality - 1;
+            } else {
+              // Cas spécial Conjured perte doublé
+              this.items[i].quality = this.items[i].quality - 2;
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
+          // Cas spéciaux gain de qualité quotidien (Brie + BackStage)
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
+            // Cas BackStage
+            if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+              if (this.items[i].sellIn < 11) {
+                if (this.items[i].quality < 50) {
+                  this.items[i].quality = this.items[i].quality + 1;
+                }
+              }
+              if (this.items[i].sellIn < 6) {
+                if (this.items[i].quality < 50) {
+                  this.items[i].quality = this.items[i].quality + 1;
+                }
+              }
+            }
+          }
+        }
+        // Ca communs SellIn < 0
+        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          this.items[i].sellIn = this.items[i].sellIn - 1;
+        }
+        // Cas perte qualité supplémentaire suite a péremption
+        if (this.items[i].sellIn < 0) {
+          if (this.items[i].name != 'Aged Brie') {
+            if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+              if (this.items[i].quality > 0) {
+                if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                  if(this.items[i].name != 'Conjured'){
+                    this.items[i].quality = this.items[i].quality - 1;
+                  }else{
+                    //Cas Conjured perte double
+                    this.items[i].quality = this.items[i].quality - 2;
+                  }
+                }
+              }
+            } else {
+              // Cas spécial BackStage après concert
+              this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            }
+          } else {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1;
+            }
           }
         }
       }
-    }
 
-    return this.items;
+      return this.items;
+    }
   }
 }
 
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,18 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  it("test item Conjured when sellIn > 0, should return quality 28, sellIn 29 and name Conjured", function() {
+    const gildedRose = new Shop([new Item("Conjured", 30, 30)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("Conjured");
+    expect(items[0].sellIn).toBe(29);
+    expect(items[0].quality).toBe(28);
+  });
+  it("test item Conjured when sellIn < 0, should return quality 28, sellIn 29 and name Conjured", function() {
+    const gildedRose = new Shop([new Item("Conjured", 0, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Conjured");
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(16);
   });
 });
